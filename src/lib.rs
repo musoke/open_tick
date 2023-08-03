bed68583eefea0e952691a754074dde0b55940a3
Nathan Musoke <nathan.musoke@gmail.com>
2023-08-03 17:27:16 -0400

    basic tick and conversion from mountain project

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,65 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use chrono::NaiveDate;
+
+pub mod mountain_project;
+use mountain_project::MountainProjectTick;
+
+/// A tick
+///
+/// This struct is non-exhaustive; it will likely gain more fields in future.
+/// # Examples
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct OpenTick<'a> {
+    /// Date the climbing happened
+    ///
+    /// May be extended in future to account for multi-day ascents and more precise times.
+    pub date: Option<NaiveDate>,
+    /// Name of the route
+    pub route_name: Option<&'a str>,
+    /// Location of the route
+    pub route_location: Option<&'a str>,
+    /// Type of route as most often climbed
+    pub route_discipline: Option<&'a str>,
+    /// Type of route as climbed in this ascent
+    pub ascent_discipline: Option<&'a str>,
+    /// Consensus grade of the route
+    pub route_grade: Option<&'a str>,
+    /// Personal grade, for this ascent
+    pub ascent_grade: Option<&'a str>,
+    /// Free-form comments
+    pub comment: Option<&'a str>,
+}
+
+impl<'a> TryFrom<MountainProjectTick<'a>> for OpenTick<'a> {
+    type Error = ConversionError;
+
+    fn try_from(value: MountainProjectTick<'a>) -> Result<Self, Self::Error> {
+        let date = value.date;
+        let route_name = Some(value.route);
+        let route_location = Some(value.location);
+        let route_discipline = Some(value.route_type);
+        let ascent_discipline = None;
+        let route_grade = Some(value.rating);
+        let ascent_grade = Some(value.your_rating);
+        let comment = Some(value.notes);
+
+        Ok(OpenTick {
+            date,
+            route_name,
+            route_location,
+            route_discipline,
+            ascent_discipline,
+            route_grade,
+            ascent_grade,
+            comment,
+        })
+    }
+}
+
+/// Errors in conversion of ticks
+#[non_exhaustive]
+pub enum ConversionError {
+    MountainProjectConversionError,
 }
 
 #[cfg(test)]
@@ -7,8 +67,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn a_tick() {
+        let t = OpenTick {
+            date: NaiveDate::from_ymd_opt(2020, 1, 1),
+            route_name: Some("A Route Name"),
+            route_location: Some("Crag Name"),
+            route_discipline: Some("aid"),
+            ascent_discipline: Some("trad"),
+            route_grade: Some("C3"),
+            ascent_grade: Some("5.11"),
+            comment: Some("What a fun route"),
+        };
+
+        println!("{t:?}")
     }
 }
---
+++ src/mountain_project.rs
@@ -0,0 +1,178 @@
+use chrono::NaiveDate;
+use std::convert::TryFrom;
+use url::Url;
+
+/// A tick as recorded in an export from
+/// `https://www.mountainproject.com/user/<userid>/<username>/tick-export`
+#[derive(Debug)]
+pub struct MountainProjectTick<'a> {
+    pub date: Option<NaiveDate>,
+    /// Mountain Project assigned name
+    pub route: &'a str,
+    /// Mountain Project assigned grade
+    pub rating: &'a str,
+    pub notes: &'a str,
+    /// URL of route on <https://www.mountainproject.com>
+    pub url: Option<Url>,
+    pub pitches: u8,
+    pub location: &'a str,
+    pub avg_stars: f32,
+    /// -1 if no rating, 1-5 otherwise
+    pub your_stars: i8,
+    pub style: MountainProjectStyle,
+    pub lead_style: Option<MountainProjectLeadStyle>,
+    /// The type of route climbed
+    /// ### Examples
+    /// ```txt
+    /// Sport
+    /// "Sport, TR"
+    /// ```
+    pub route_type: &'a str,
+    /// ticker's own grade, which may differ from "official" grade
+    pub your_rating: &'a str,
+    /// length of route in feet
+    pub length: usize,
+    /// unclear meaning, u16 might suffice
+    pub rating_code: u32,
+}
+
+/// Styles of ascent allowed by Mountain Project
+#[non_exhaustive]
+#[derive(Debug, PartialEq)]
+pub enum MountainProjectStyle {
+    /// only for boulders
+    Attempt,
+    /// only for boulders
+    Flash,
+    /// not for boulders
+    Follow,
+    /// not for boulders
+    Lead,
+    /// only for boulders
+    Send,
+    /// not for boulders
+    Solo,
+    /// not for boulders
+    TR,
+}
+
+/// Sub-styles for lead ascents
+#[non_exhaustive]
+#[derive(Debug, PartialEq)]
+pub enum MountainProjectLeadStyle {
+    FellHung,
+    Flash,
+    Onsight,
+    Pinkpoint,
+    Redpoint,
+}
+
+/// ID of a route in Mountain Project's database
+///
+/// # Examples
+///
+/// ```
+/// use url::Url;
+/// use open_tick::mountain_project::MountainProjectRouteId;
+/// let url = Url::parse("https://www.mountainproject.com/route/12321/route-name")
+/// .ok()
+/// .unwrap();
+/// let mp_id = MountainProjectRouteId::try_from(url).unwrap();
+/// assert_eq!(mp_id.0, 12321)
+/// ```
+#[derive(Debug, PartialEq)]
+pub struct MountainProjectRouteId(pub usize);
+
+impl TryFrom<Url> for MountainProjectRouteId {
+    type Error = MountainProjectIdConversionError;
+
+    fn try_from(value: Url) -> Result<Self, Self::Error> {
+        if value.domain() != Some("www.mountainproject.com") {
+            Err(MountainProjectIdConversionError::WrongDomain)
+        } else {
+            let mut path = value
+                .path_segments()
+                .ok_or(MountainProjectIdConversionError::WrongDomain)?;
+
+            match path.next() {
+                Some("route") => {}
+                // "v" is another possibly valid value, but then don't know if it's a route or area
+                Some("v") => Err(MountainProjectIdConversionError::BadPath)?,
+                Some(_) => Err(MountainProjectIdConversionError::BadPath)?,
+                None => Err(MountainProjectIdConversionError::BadPath)?,
+            }
+
+            let id = path
+                .next()
+                .ok_or(MountainProjectIdConversionError::BadPath)?
+                .parse::<usize>()
+                .map_err(|_| -> MountainProjectIdConversionError {
+                    MountainProjectIdConversionError::BadPath
+                })?;
+
+            Ok(MountainProjectRouteId(id))
+        }
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug, PartialEq)]
+pub enum MountainProjectIdConversionError {
+    WrongDomain,
+    BadPath,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn good_mp_url() {
+        let id = 123456;
+        let url = Url::parse(&format!(
+            "https://www.mountainproject.com/route/{id}/route-name"
+        ))
+        .ok()
+        .unwrap();
+        let mp_id = MountainProjectRouteId::try_from(url);
+
+        assert_eq!(mp_id, Ok(MountainProjectRouteId(id)))
+    }
+
+    #[test]
+    fn bad_mp_route_url() {
+        let id = 123456;
+        let url = Url::parse(&format!(
+            "https://www.mountainproject.com/area/{id}/area-name"
+        ))
+        .ok()
+        .unwrap();
+        let mp_id = MountainProjectRouteId::try_from(url);
+
+        assert!(mp_id.is_err())
+    }
+
+    #[test]
+    fn a_tick() {
+        let t: MountainProjectTick = MountainProjectTick {
+            date: NaiveDate::from_ymd_opt(2020, 1, 1),
+            route: "a route name",
+            rating: "V2",
+            notes: "fund route",
+            url: Url::parse("https://www.mountainproject.com/route/123456/route-name").ok(),
+            pitches: 1,
+            location: "A place > the crag",
+            avg_stars: 3.2,
+            your_stars: 3,
+            style: MountainProjectStyle::TR,
+            lead_style: Some(MountainProjectLeadStyle::FellHung),
+            route_type: "\"Trad, TR\"",
+            your_rating: "5.10",
+            length: 10,
+            rating_code: 20008,
+        };
+
+        println!("{t:?}")
+    }
+}
